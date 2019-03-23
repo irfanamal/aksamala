fedf3f7b77670e0ed898a591e8743605a1eb483b
irfanamal <irfan.ihsanulamal@yahoo.com>
2019-03-23 17:52:33 +0700

    add PlayerMove and CameraFollow script

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Camera mainCamera;
+    private Transform playerTransform;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        playerTransform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mainCamera.transform.position = new Vector3(playerTransform.position.x + 2.0f, playerTransform.position.y, playerTransform.position.z - 4.0f);
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector3(moveHorizontal, 0.0f, moveVertical) * speed;
+    }
+}
